6c8a569d561b79aa95f147e07126051f9225ea6b
Ranjithgc <Ranjith.amca.16@acharya.ac.in>
2021-07-05 00:49:53 +0530

    [Ranjithgc] Refactor: to convert list into nested dictionary of keys

---
+++ Data Structures/Dictionaries/list_dictionary.py
@@ -0,0 +1,27 @@
+'''
+@Author: Ranjith G C
+@Date: 2021-07-04
+@Last Modified by: Ranjith G C
+@Last Modified time: 2021-07-04 
+@Title : Program Aim to convert list into nested dictionary of keys
+'''
+
+from loggers import logger
+
+def convert_to_dictionary():
+    '''
+    Description:
+        This function converts list into nested
+        dictionary of keys
+    '''
+    num_list = [1, 2, 3, 4]
+    new_dict = current = {}
+    
+    for name in num_list:
+        current[name] = {}
+        current = current[name]
+    
+    logger.info(new_dict)
+
+if __name__ == "__main__":
+    convert_to_dictionary()
