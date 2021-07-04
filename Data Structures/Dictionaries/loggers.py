139cf99296acb55a37749d86f55cb35727a48fc8
Ranjithgc <Ranjith.amca.16@acharya.ac.in>
2021-07-04 22:06:38 +0530

    [Ranjithgc] Refactor: method and variables

--- Data Structures/Dictionaries/loggers.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-logger = logging
-   
-# logging basic config method and saving log files
-logger.basicConfig(filename='dictinary.log', level=logging.INFO)
-logger.basicConfig(filename='dictionary.log', level=logging.ERROR)
