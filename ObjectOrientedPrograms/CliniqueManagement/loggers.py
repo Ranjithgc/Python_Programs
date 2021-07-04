e1f6cd5d583f4d6e40d0b2ae4032293a7563af83
Ranjithgc <Ranjith.amca.16@acharya.ac.in>
2021-07-04 17:51:06 +0530

    [Ranjithgc] Refactor: Method names and variables

--- ObjectOrientedPrograms/CliniqueManagement/loggers.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-logger = logging
-   
-# logging basic config method and saving log files
-logger.basicConfig(filename='addressbook.log', level=logging.INFO)
-logger.basicConfig(filename='addressbook.log', level=logging.ERROR)
