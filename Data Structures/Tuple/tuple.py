8812d7a1c334f8daf42367cc73b15b781acae71e
Ranjithgc <Ranjith.amca.16@acharya.ac.in>
2021-07-06 00:37:51 +0530

    [Ranjithgc] Add: Created a tuple

---
+++ Data Structures/Tuple/tuple.py
@@ -0,0 +1,17 @@
+'''
+@Author: Ranjith G C
+@Date: 2021-07-05
+@Last Modified by: Ranjith G C
+@Last Modified time: 2021-07-05 
+@Title : Program Aim to Append a list to the second list 
+'''
+from loggers import logger
+
+
+#Create an empty tuple 
+x = ()
+logger.info(x)
+
+#Create an empty tuple with tuple() function built-in Python
+tuplex = tuple({1,2,3,4,5})
+logger.info(tuplex)
