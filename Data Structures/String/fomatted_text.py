0bd2387208d3deafbfeec067411b97202ffb34b8
Ranjithgc <Ranjith.amca.16@acharya.ac.in>
2021-07-06 21:26:01 +0530

    [Ranjithgc] Add: Display formatted text

---
+++ Data Structures/String/fomatted_text.py
@@ -0,0 +1,23 @@
+'''
+@Author: Ranjith G C
+@Date: 2021-07-06
+@Last Modified by: Ranjith G C
+@Last Modified time: 2021-07-06 
+@Title : Program Aim to Display Formatted Text
+'''
+from loggers import logger
+
+import textwrap
+
+try:
+    sample_text = '''
+    Python is a widely used high-level, general-purpose, interpreted,
+    dynamic programming language. Its design philosophy emphasizes
+    code readability, and its syntax allows programmers to express
+    concepts in fewer lines of code than possible in languages such
+    as C++ or Java.
+  '''
+    print("{}={}".format(sample_text, 50))
+
+except Exception:
+    logger.error("Invalid Text")
